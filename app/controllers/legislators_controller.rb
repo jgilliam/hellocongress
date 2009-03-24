7694d9f274253c5a043a06619a62a5d3dcf40d30
Jim Gilliam <jim@gilliam.com>
2009-03-23 17:34:11 -0700

    added wk/mo trending to priority list, added a little css formatting

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -54,7 +54,7 @@ class LegislatorsController < ApplicationController
   def show
     @per_page = 25
     @legislator = Legislator.find(params[:id])
-    @page_title = "Hello, " + @legislator.lastname_with_title + '.'
+    @page_title = "Hello, " + @legislator.name_with_title + "."
     @priorities = @legislator.priorities.by_position.paginate :page => params[:page], :per_page => @per_page
     respond_to do |format|
       format.html # show.html.erb
--- app/models/priority.rb
+++ app/models/priority.rb
@@ -17,5 +17,29 @@ class Priority < ActiveRecord::Base
       self.url += '-' + self.name.gsub(/[^a-z0-9]+/i, '-').downcase
     end
   end
+  
+  def status_name
+    return 'failed' if obama_status == -2
+    return 'compromised' if obama_status == -1
+    return 'unknown' if obama_status == 0 
+    return 'in the works' if obama_status == 1
+    return 'successful' if obama_status == 2
+  end
+
+  def is_failed?
+    obama_status == -2
+  end
+  
+  def is_successful?
+    obama_status == 2
+  end
+  
+  def is_compromised?
+    obama_status == -1
+  end
+  
+  def is_intheworks?
+    obama_status == 1
+  end
 
 end
