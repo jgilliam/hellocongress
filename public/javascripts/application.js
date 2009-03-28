e1b2aad281db466e337b6f948bfb2c9865f63199
Jim Gilliam <jim@gilliam.com>
2009-03-28 15:52:22 -0700

    homepage work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -28,6 +28,17 @@ jQuery(document).ready(function() {
 		fill: '#ffff99',
 		strokeWidth: 1,
 		strokeStyle: '#ffcc00',
+	});
+	$('input#hp_search').bt({ 
+		positions: ['bottom'],
+		padding: 10,
+		width: 140,
+		spikeLength: 20,
+		spikeGirth: 20,
+		cornerRadius: 20,
+		fill: '#ffff99',
+		strokeWidth: 1,
+		strokeStyle: '#ffcc00',
 	});	
 	$('input.bt').bt({ 
 		trigger: ['focus', 'blur'], 
