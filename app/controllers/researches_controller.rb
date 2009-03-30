a0732d703bb39a0f63e634877467a920c72e29d8
Jim Gilliam <jim@gilliam.com>
2009-03-30 11:28:22 -0700

    rss and xml feeds for research

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,12 +1,15 @@
 class ResearchesController < ApplicationController
+
   # GET /researches
   # GET /researches.xml
   def index
+    @page_title = "Hello Congress research requests"
     @researches = Research.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @researches }
+      format.xml  { render :xml => @researches.to_xml(:except => [:email]) }
+      format.rss
     end
   end
 
@@ -14,10 +17,10 @@ class ResearchesController < ApplicationController
   # GET /researches/1.xml
   def show
     @research = Research.find(params[:id])
-
+    @page_title = "Hello Congress research request: " + @research.name
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @research }
+      format.xml  { render :xml => @research.to_xml(:except => [:email]) }
     end
   end
 
@@ -28,13 +31,8 @@ class ResearchesController < ApplicationController
     @page_title = "Request research from White House 2 members"
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @research }
+      format.xml  { render :xml => @research.to_xml(:except => [:email]) }
     end
-  end
-
-  # GET /researches/1/edit
-  def edit
-    @research = Research.find(params[:id])
   end
 
   # POST /researches
@@ -54,32 +52,4 @@ class ResearchesController < ApplicationController
     end
   end
 
-  # PUT /researches/1
-  # PUT /researches/1.xml
-  def update
-    @research = Research.find(params[:id])
-
-    respond_to do |format|
-      if @research.update_attributes(params[:research])
-        flash[:notice] = 'Research was successfully updated.'
-        format.html { redirect_to(@research) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @research.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-  
-  # DELETE /researches/1
-  # DELETE /researches/1.xml
-  def destroy
-    @research = Research.find(params[:id])
-    @research.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(researches_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
