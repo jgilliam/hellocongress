1ac2e832f73a7f875a275afd72921c1474284a5b
Jim Gilliam <jim@gilliam.com>
2009-03-30 18:30:08 -0700

    added legistalker to contact links for each legislator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,7 @@ module ApplicationHelper
     r = []
     r << '<a href="http://twitter.com/' + legislator.twitter_id + '">Twitter</a>' if legislator.attribute_present?("twitter_id")
     r << '<a href="' + legislator.youtube_url + '">YouTube</a>' if legislator.attribute_present?("youtube_url")    
+    r << '<a href="http://legistalker.org/people/' + legislator.bioguide_id + '">Legistalker</a>' if legislator.attribute_present?("bioguide_id")
     r << '<a href="' + legislator.congresspedia_url + '">OpenCongress</a>' if legislator.attribute_present?("congresspedia_url")
     r << '<a href="http://www.govtrack.us/congress/person.xpd?id=' + legislator.govtrack_id.to_s + '">GovTrack</a>' if legislator.attribute_present?("govtrack_id")
     r.to_sentence + "."
